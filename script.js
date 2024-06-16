eead245787a19032c385f6db51a9b64c45840779
Riku123123123 <145870211+Riku123123123@users.noreply.github.com>
2024-06-16 19:06:41 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Lisätään klikkikuuntelija jokaiseen boksin otsikkoon
-document.querySelectorAll('.box-trigger').forEach(item => {
-    item.addEventListener('click', event => {
-        const box = item.parentElement;
-        const content = box.querySelector('.box-content');
-        box.classList.toggle('active');
-        content.classList.toggle('active');
-    });
-});
