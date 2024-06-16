9dabb564f7f2866e7af49efae75b35d5cfcc6239
Riku123123123 <145870211+Riku123123123@users.noreply.github.com>
2024-06-16 19:17:06 +0300

    Add files via upload

--- JS/GuideJS.js
+++ JS/GuideJS.js
@@ -1,1 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let navigationHistory = [];
 
+    function navigateTo(sectionId) {
+        const sections = document.querySelectorAll('section');
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+        const targetSection = document.getElementById(sectionId);
+        if (targetSection) {
+            targetSection.style.display = 'block';
+            window.scrollTo(0, 0); // Siirrä näkymä sivun alkuun
+        }
+    
+        if (navigationHistory.length === 0 || navigationHistory[navigationHistory.length - 1] !== sectionId) {
+            navigationHistory.push(sectionId); // Lisää osio historiaan
+        }
+    }
+    
+
+    function navigateBack() {
+        if (navigationHistory.length > 1) {
+            navigationHistory.pop();
+            const previousSection = navigationHistory[navigationHistory.length - 1];
+            navigateTo(previousSection);
+        }
+    }
+
+    window.navigateTo = navigateTo;
+    window.navigateBack = navigateBack;
+
+    navigateTo('home');
+});
+
+function navigateToPage(url) {
+    window.location.href = url;
+}
---
+++ JS/PrivacyJS.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Estää automaattisen siirtymisen alaspäin sivustoa avatessa
+    window.scrollTo(0, 0);
+    
+    // Navigaatio-toiminto
+    function navigateTo(sectionId) {
+        const sections = document.querySelectorAll('section');
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+        const targetSection = document.getElementById(sectionId);
+        if (targetSection) {
+            targetSection.style.display = 'block';
+            window.scrollTo(0, 0); // Estää automaattisen siirtymisen alaspäin navigoidessa
+        }
+    }
+
+    // Avaa aloitusosio
+    navigateTo('privacy');
+
+    // Kiinnitetään funktiot globaaleihin muuttujiin, jotta ne ovat saatavilla HTML:stä
+    window.setLanguage = setLanguage;
+    window.navigateTo = navigateTo;
+    window.toggleMenu = toggleMenu;
+});
+
+function navigateToPage(url) {
+    window.location.href = url;
+}
